7c61422c9e3a23e622858a1647403a22d79776d6
Aaron Keohane <75237371+aakeohane@users.noreply.github.com>
2021-08-12 01:00:32 -0700

    add hamburger menu

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Opens hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navList.classList.toggle("active");
+}
+
+// Closes Hamburger Menu
+const navLink = document.querySelectorAll("navigation-list__item");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
